1878dc1bb880addccd95d0b80f3551ba7a2d8b3b
Danvintius <danila.94@mail.ru>
2023-09-18 00:04:03 +0300

    1

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        final var validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+
+        Server server = new Server();
+    }
+
+}
